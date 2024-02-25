bfc1971fce7c0ebcface55a94b639c1db1167377
Денис <baden.i.ua@gmail.com>
2024-02-25 14:46:18 +0200

    Вау! Працює. RGB світлодіод мерехтить.

--- app/src/main.c
+++ app/src/main.c
@@ -1,48 +1,84 @@
 /*
- * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2017 Linaro Limited
+ * Copyright (c) 2018 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#define LOG_LEVEL 4
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main);
+
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/led_strip.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/sys/util.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define STRIP_NODE		DT_ALIAS(led_strip)
+#define STRIP_NUM_PIXELS	DT_PROP(DT_ALIAS(led_strip), chain_length)
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define DELAY_TIME K_MSEC(CONFIG_SAMPLE_LED_UPDATE_DELAY)
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
+
+static const struct led_rgb colors[] = {
+	RGB(0x0f, 0x00, 0x00), /* red */
+	RGB(0x00, 0x0f, 0x00), /* green */
+	RGB(0x00, 0x00, 0x0f), /* blue */
+};
+
+struct led_rgb pixels[STRIP_NUM_PIXELS];
+
+static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 
 int main(void)
 {
-	int ret;
-	bool led_state = true;
+	size_t cursor = 0, color = 0;
+	int rc;
 
-	if (!gpio_is_ready_dt(&led)) {
+	if (device_is_ready(strip)) {
+		LOG_INF("Found LED strip device %s", strip->name);
+	} else {
+		LOG_ERR("LED strip device %s is not ready", strip->name);
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
-
+	LOG_INF("Displaying pattern on strip");
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
+		memset(&pixels, 0x00, sizeof(pixels));
+		memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
+		rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
+
+		if (rc) {
+			LOG_ERR("couldn't update strip: %d", rc);
 		}
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
+		cursor++;
+		if (cursor >= STRIP_NUM_PIXELS) {
+			cursor = 0;
+			color++;
+			if (color == ARRAY_SIZE(colors)) {
+				color = 0;
+			}
+		}
+
+		k_sleep(DELAY_TIME);
 	}
 	return 0;
 }
+
+// #include <zephyr/kernel.h>
+
+// int main(void)
+// {
+//     int count = 0;
+//     for(;;) {
+//         printk("Hello World %d! %s\n", count++, CONFIG_BOARD);
+//         k_sleep(K_MSEC(1000));
+//     }
+//     return 0;
+// }
